011391fc387c8e297637d45fda0ed75f68d7aade
hojooneum <bldolphin96@gmail.com>
2022-10-03 19:55:52 +0900

    add Date class test

--- IIKH_test/IIKH_test.cpp
+++ IIKH_test/IIKH_test.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "../IIKH_win/Date.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -11,7 +12,11 @@ namespace IIKHtest
 		
 		TEST_METHOD(TestMethod1)
 		{
-			Assert::AreEqual(1, 1);
+			Date dateA = Date(2022, 10, 1);
+			Date dateB = Date(2022, 10, 1);
+			int yearA = dateA.getYear();
+			int yearB = dateB.getYear();
+			Assert::AreEqual(yearA, yearB);
 		}
 	};
 }
