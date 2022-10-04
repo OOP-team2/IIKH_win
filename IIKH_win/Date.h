d50a9acea6a96e42c757e1f09ac9771a88ddc66b
hojooneum <bldolphin96@gmail.com>
2022-10-04 23:54:39 +0900

    update Date Class member variable, functions

--- IIKH_win/Date.h
+++ IIKH_win/Date.h
@@ -3,21 +3,38 @@
 //
 
 #pragma once
+#include "string"
 
+// a class for date
 class Date {
 private:
-    int year;
-    int month;
-    int day;
+    // a string variable for a year
+    std::string year;
+    // a string variable for a month
+    std::string month;
+    // a string variable for a day
+    std::string day;
 public:
+    // a default confructor for Date
     Date();
-    Date(int new_year, int new_month, int new_day);
-    int getYear();
-    void setYear(int new_year);
-    int getMonth();
-    void setMonth(int new_month);
-    int getDay();
-    void setDay(int new_day);
+    // a constructor with parameter
+    Date(std::string new_year, std::string new_month, std::string new_day);
+    // a getter for year
+    std::string getYear();
+    // a setter for year
+    void setYear(std::string new_year);
+    // a getter for month
+    std::string getMonth();
+    // a setter for month
+    void setMonth(std::string new_month);
+    // a getter for day
+    std::string getDay();
+    // a setter fot day
+    void setDay(std::string new_day);
+    // a function for displaying date information and editing date information
+    void displayAndEdit();
+    // a function for building grocery list
+    void buildGroceryList();
 
     bool operator== (Date otherDate);
 };
